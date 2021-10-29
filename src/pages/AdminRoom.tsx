8f6d786ab23356ad9a34ef9b5108320f8dae2823
Makley Tibola Trichez <tibolamakley1@gmail.com>
2021-10-29 15:49:23 -0300

    Exploring the surface

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -8,6 +8,9 @@ import { useRoom } from "../hooks/useRoom";
 
 import "../styles/room.scss";
 import deleteImg from "../assets/images/delete.svg";
+import checkImg from "../assets/images/check.svg";
+import answerImg from "../assets/images/answer.svg";
+
 import { database } from "../services/firebase";
 type RoomParams = {
 	id: string;
@@ -32,6 +35,17 @@ export default function AdminRoom() {
 		}
 
 		history.push("/");
+	}
+
+	async function handleCheckQuestionAsAnswered(questionId: string) {
+		await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+			isAnswered: true,
+		});
+	}
+	async function handleHighlightQuestion(questionId: string) {
+		await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+			isHighlighted: true,
+		});
 	}
 
 	return (
@@ -60,12 +74,30 @@ export default function AdminRoom() {
 							key={question.id}
 							content={question.content}
 							author={question.author}
+							isAnswered={question.isAnswered}
+							isHighlighted={question.isHighlighted}
 						>
+							{!question.isAnswered && (
+								<>
+									<button
+										type="button"
+										onClick={() => handleCheckQuestionAsAnswered(question.id)}
+									>
+										<img src={checkImg} alt="Marcar pergunta como respondida" />
+									</button>
+									<button
+										type="button"
+										onClick={() => handleHighlightQuestion(question.id)}
+									>
+										<img src={answerImg} alt="Dar destaque à pergunta" />
+									</button>
+								</>
+							)}
 							<button
 								type="button"
 								onClick={() => handleDeleteQuestion(question.id)}
 							>
-								<img src={deleteImg} alt="" />
+								<img src={deleteImg} alt="Delete question" />
 							</button>
 						</Question>
 					))}
